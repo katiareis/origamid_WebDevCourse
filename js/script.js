b4a7fdb28eff6eab11e153fb0bbf7bc6b31b93a5
Katia Reis <62106987+katiareis@users.noreply.github.com>
2021-06-26 17:07:55 -0300

    Add PHP files to website and form data.
    Project Update - Add PHP files to project and some HTML form data.

--- js/script.js
+++ js/script.js
@@ -18,3 +18,13 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
 	new SimpleAnime();
 }
+
+// Envio de formulário
+if(window.SimpleForm) {
+	new SimpleForm({
+	  form: ".form_php", // seletor do formulário
+	  button: "#enviar", // seletor do botão
+	  erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // mensagem de erro
+	  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+	});
+}
